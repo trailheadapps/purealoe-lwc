8a3ad0d542cdf8d5d17beedc5fc5d050c8ba33d6
satyasekharcvb <31529847+satyasekharcvb@users.noreply.github.com>
2019-10-15 07:44:48 +0530

    migrating aura flow wrapper to lwc (#51)
    * Weekly CI builds (#46)
    
    * Weekly CI builds (#47)
    
    * migrating aura flow wrapper to lwc
    
    * prettier format the file irrigationManager.js

--- force-app/main/default/lwc/irrigationManager/irrigationManager.js
+++ force-app/main/default/lwc/irrigationManager/irrigationManager.js
@@ -1,5 +1,6 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 import FIELD_ID from '@salesforce/schema/Harvest_Field__c.Id';
 import FIELD_NAME from '@salesforce/schema/Harvest_Field__c.Name';
 import FIELD_STATUS from '@salesforce/schema/Harvest_Field__c.Status__c';
@@ -15,39 +16,67 @@ const fields = [
 ];
 
 export default class IrrigationManager extends LightningElement {
+    //Flow input properties
     @api duration;
-    @api ldsRecId;
-    @api defaultMsg;
-    @api overrideCount;
+    @api recordId;
     @api startTime;
+
+    //Flow output property
+    @api addedMsgText;
 
     @track messageText;
 
     @wire(getRecord, {
-        recordId: '$ldsRecId',
+        recordId: '$recordId',
         fields
     })
     harvestRecord;
 
+    //Flow output property
+    @api
+    get overrideCount() {
+        return getFieldValue(
+            this.harvestRecord.data,
+            FIELD_IRRIGATION_OVERRIDES
+        );
+    }
+
+    @api
+    get defaultMsg() {
+        const name = getFieldValue(this.harvestRecord.data, FIELD_NAME);
+        return (
+            'Alert! Irrigation for ' +
+            name +
+            ' will begin at ' +
+            this.startTime +
+            ', and is scheduled to last ' +
+            this.duration +
+            ' minutes.'
+        );
+    }
+
     handleStartTimeChange(event) {
-        const changeEvent = new CustomEvent('flowdatachanged', {
-            detail: { startTime: event.target.value }
-        });
-        this.dispatchEvent(changeEvent);
+        const attributeChangeEvent = new FlowAttributeChangeEvent(
+            'startTime',
+            event.target.value
+        );
+        this.dispatchEvent(attributeChangeEvent);
     }
 
     handleDurationChange(event) {
-        const changeEvent = new CustomEvent('flowdatachanged', {
-            detail: { duration: event.target.value }
-        });
-        this.dispatchEvent(changeEvent);
+        const attributeChangeEvent = new FlowAttributeChangeEvent(
+            'duration',
+            event.target.value
+        );
+        this.dispatchEvent(attributeChangeEvent);
     }
 
     handleMessageChange(event) {
         this.messageText = event.target.value;
-        const changeEvent = new CustomEvent('flowdatachanged', {
-            detail: { addedMsgText: event.target.value }
-        });
-        this.dispatchEvent(changeEvent);
+        const attributeChangeEvent = new FlowAttributeChangeEvent(
+            'addedMsgText',
+            event.target.value
+        );
+        this.dispatchEvent(attributeChangeEvent);
     }
 }
