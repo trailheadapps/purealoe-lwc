e0dbacb658e1ecee39c5ad61d07d17ad7234fa4e
satyasekharcvb <ssekhar@salesforce.com>
2020-02-13 22:07:28 +0530

    Removed unused field

--- force-app/main/default/lwc/harvestFieldMapNav/harvestFieldMapNav.js
+++ force-app/main/default/lwc/harvestFieldMapNav/harvestFieldMapNav.js
@@ -28,7 +28,6 @@ export default class HarvestFieldMapNav extends NavigationMixin(
     @api recordId;
     error;
     harvestField;
-    harvestFields;
 
     @wire(getRecord, {
         recordId: '$recordId',
